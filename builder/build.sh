696c2288aa89e8a88df4f246fa3d4754217b7338
CrazyChenz <crazychenz@gmail.com>
2022-03-26 13:37:36 -0400

    Removed builder and added gitignore files

--- builder/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-docker build $1 -t crazychenz/rpi4-kbuild context
--- builder/context/build-initramfs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd rootfs
-# Note: busybox may have created some of these during its install.
-mkdir -p dev bin sbin etc proc sys usr/bin usr/sbin
-mknod -m 622 ./dev/console c 5 1
-mknod -m 666 ./dev/null c 1 3
-find . -print0 | cpio --null -ov --format=newc | gzip -9 > ../initramfs.cpio.gz
-cd ..
